118467f79382db9cdef723f9416f94ae4839a8ec
NickShelud <N.She.1997@mail.ru>
2022-10-05 08:24:54 +0300

    update bin/brain-games

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function gettingName($name)
+function gettingName()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
---
+++ src/Even.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BrainGames\Even;
+
+function randArr()
+{
+    for ($i = 0; $i <= 3; $i++) {
+        
+    }
+}
