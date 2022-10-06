7a0cf187add8386b4d3fd33b6c079d684627c7d4
NickShelud <N.She.1997@mail.ru>
2022-10-06 08:41:25 +0300

    Step 5

--- src/Cli.php
+++ src/Cli.php
@@ -10,4 +10,5 @@ function gettingName()
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
--- src/Even.php
+++ src/Even.php
@@ -2,9 +2,52 @@
 
 namespace BrainGames\Even;
 
-function randArr()
+use function cli\line;
+use function cli\prompt;
+
+function createRandArr()
 {
-    for ($i = 0; $i <= 3; $i++) {
-        
+    for ($i = 0; $i < 3; $i++) {
+        $arr[] = rand(1, 100);
+    };
+    return $arr;
+}
+//$randArr = createRandArr();
+
+function checkForParity($randArr)
+{
+    foreach ($randArr as $item) {
+        if ((int) $item % 2 == 0) {
+            $correctAnswerArr[] = 'yes';
+        } else {
+            $correctAnswerArr[] = 'no';
+        }
+    }
+    return $correctAnswerArr;
+}
+//$correctAnswerArr = checkForParity($randArr);
+
+function checkAnswers($name)
+{
+    $randArr = createRandArr();
+    $correctAnswerArr = checkForParity($randArr);
+    $counter = 0;
+    for ($i = 0; $i < 3; $i++) {
+        line("{$randArr[$i]}");
+        $answer = prompt('Your answer');
+        if ($answer === $correctAnswerArr[$i]) {
+            line('Correct');
+            $counter++;
+        } elseif ($answer === 'no' and $correctAnswerArr[$i] === 'yes') {
+            line("'no' is wrong answer ;(. Correct answer was 'yes'.");
+            exit;
+        } else {
+            line("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            exit;
+        }
+    
+    }
+    if ($counter === 3) {
+        line("Congratulations %s!", $name);
     }
 }
